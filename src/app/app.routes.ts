ce844a911eb8f1744e3c365ca8cc0ca306feefbe
Martin Reifschneider <mreifschneider@me.com>
2025-04-11 11:07:07 +0200

    create Portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StartComponent } from './start/start.component';
 import { AboutComponent } from './pages/about/about.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 export const routes: Routes = [
     { path: '', component: StartComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'skills', component: SkillsComponent},
+    { path: 'portfolio', component: PortfolioComponent}
 ];
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -4,11 +4,12 @@ import { StartCenterComponent } from './start-center/start-center.component';
 import { StartBottomComponent } from './start-bottom/start-bottom.component';
 import { AboutComponent } from '../pages/about/about.component';
 import { SkillsComponent } from '../pages/skills/skills.component';
+import { PortfolioComponent } from '../pages/portfolio/portfolio.component';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [StartTopComponent, StartCenterComponent, StartBottomComponent, AboutComponent, SkillsComponent],
+  imports: [StartTopComponent, StartCenterComponent, StartBottomComponent, AboutComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
